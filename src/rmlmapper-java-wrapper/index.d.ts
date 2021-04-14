5de8f94ac566c64636c5aea4a9624657faa84c8e
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-04-14 21:08:22 +0800

    feat: 添加 @rmlio/rmlmapper-java-wrapper 的类型定义文件

---
+++ src/rmlmapper-java-wrapper/index.d.ts
@@ -0,0 +1,4 @@
+declare module '@rmlio/rmlmapper-java-wrapper' {
+  const _exports: typeof import('./lib/wrapper');
+  export = _exports;
+}
---
+++ src/rmlmapper-java-wrapper/lib/utils.d.ts
@@ -0,0 +1,2 @@
+import N3 = require('n3');
+export function strToQuads(str: string): Promise<N3.Quad>;
